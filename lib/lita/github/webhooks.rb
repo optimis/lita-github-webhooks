a877048a1e94e8eb64893839e2001129a34d1ca0
YangHungTW <yang.hung.tw@gmail.com>
2016-03-28 18:06:34 +0800

    Git Initailize

---
+++ lib/lita/github/webhooks.rb
@@ -0,0 +1,9 @@
+require "lita/github/webhooks/version"
+
+module Lita
+  module Github
+    module Webhooks
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/lita/github/webhooks/version.rb
@@ -0,0 +1,7 @@
+module Lita
+  module Github
+    module Webhooks
+      VERSION = "0.1.0"
+    end
+  end
+end
